4b955b189fb66ef19bc80bc8a6c92466a1a85709
Mina Houshangi <minahoushangi4@gmail.com>
2024-06-20 05:11:17 -0700

    refactor(paths): use absolute paths for imports.
    - Updated project configuration to support absolute paths
    - Improved code readability and maintainability

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          src: './src',
+          '@navigation': './src/navigation',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@hooks': './src/hooks',
+          '@assets': './src/assets',
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+  ],
 };
